678ed67a5634d7aa89be53f03c08563db0cc797f
Eliza De Barros <80429458+ElizaDeBarros@users.noreply.github.com>
2025-02-14 19:39:01 -0800

    Initial commit for backend

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    today = date.today()
+    current_year = today.year
+    return render_template("index.html", current_year=current_year)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
