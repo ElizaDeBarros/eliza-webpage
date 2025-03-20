6b9e27240635493a914760b53a75bf95e3986eb0
Eliza De Barros <80429458+ElizaDeBarros@users.noreply.github.com>
2025-03-20 15:12:38 -0700

    new requirements file created

--- server.py
+++ server.py
@@ -226,4 +226,4 @@ def get_stats():
             conn.close()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
